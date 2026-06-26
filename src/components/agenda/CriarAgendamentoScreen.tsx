4cb22faa36a630f6783b6c80c7ce5fcf9b75d1f9
ativeaobsessao <gustavo.hvps@gmail.com>
2026-06-26 00:17:10 -0300

    Import React explicitly in CriarAgendamentoScreen

--- src/components/agenda/CriarAgendamentoScreen.tsx
+++ src/components/agenda/CriarAgendamentoScreen.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { useDataStore } from '../../store/useDataStore';
 import { useAuthStore } from '../../store/useAuthStore';
 import { CustomSelect } from '../ui/CustomSelect';
